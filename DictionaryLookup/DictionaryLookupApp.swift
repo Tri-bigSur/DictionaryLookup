a20b832f437cb7cdd55888869c88b38b4361ba66
Tri Vo <warbo.trivo@gmail.com>
2026-06-07 11:35:15 +0700

    Initial Commit

---
+++ DictionaryLookup/DictionaryLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryLookupApp.swift
+//  DictionaryLookup
+//
+//  Created by warbo on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

3e14efb5a6bdaeab601f0191a2df08f1e9beca26
Tri Vo <warbo.trivo@gmail.com>
2026-06-30 15:50:21 +0700

    adding text-to-speech feature and improving pressing effect for sound button

---
+++ DictionaryLookup/HapticManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticManager.swift
+//  DictionaryLookup
+//
+//  Created by warbo on 30/6/26.
+//
+
+import Foundation
---
+++ DictionaryLookup/SpeechManager.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechManager.swift
+//  DictionaryLookup
+//
+//  Created by warbo on 29/6/26.
+//
+
+import Foundation
